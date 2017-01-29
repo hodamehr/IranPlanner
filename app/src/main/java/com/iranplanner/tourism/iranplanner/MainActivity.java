9ac681ece65177f3e6768a05e87788598d402658
Hoda <vahidimehr@gmail.com>
2017-01-29 12:30:46 -0800

    set toolbar custom

--- app/src/main/java/com/iranplanner/tourism/iranplanner/MainActivity.java
+++ app/src/main/java/com/iranplanner/tourism/iranplanner/MainActivity.java
@@ -4,6 +4,10 @@ package com.iranplanner.tourism.iranplanner;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+
 
 import com.iranplanner.tourism.iranplanner.adapter.TabPagerAdapter;
 import com.iranplanner.tourism.iranplanner.standard.StandardActivity;
@@ -20,6 +24,24 @@ public class MainActivity extends StandardActivity {
 //        test test=new test();
 //        test.getItinerary("342");
         // Setup the viewPager
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        View logo = getLayoutInflater().inflate(R.layout.custom_imageview_toolbar, null);
+        toolbar.addView(logo);
+
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setIcon(R.mipmap.ic_air_gret);
+
+
+//        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayShowCustomEnabled(true);
+//
+//        LayoutInflater inflator = (LayoutInflater) this .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        View v = inflator.inflate(R.layout.custom_imageview_toolbar, null);
+//
+//        actionBar.setCustomView(v);
+//        actionBar.setIcon(R.drawable.emam2);
+
         ViewPager viewPager = (ViewPager) findViewById(R.id.main_view_pager);
         TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(),this);
         if (viewPager != null)
