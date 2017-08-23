225d1d0210158b935e6eca3a5390dd9862755b23
hodamehr <vahidimehr@gmail.com>
2017-08-23 12:28:04 +0430

    constants update

--- app/src/main/java/tools/Constants.java
+++ app/src/main/java/tools/Constants.java
@@ -16,4 +16,13 @@ public final class Constants {
     public static final String wishImg = "1";
     public static final String homeSearch = "homeSearch";
     public static final String homeHotel = "homeHotel";
+    public static final String hotelCode = "147";
+    public static final String hotelApartmentCode = "3762";
+    public static final String hotelboomgardiCode = "3795";
+    public static final String hotelTraditionalCode = "148";
+    public static final String attractionRelegonCode = "2930";
+    public static final String attractionNaturalCode = "2931";
+    public static final String attractionHistoricalCode = "2932";
+    public static final String attractionSortCode = "2934";
+    public static final int durationTravel = 3;
 }
